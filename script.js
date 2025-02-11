f4ba121a607d3b8ea4c70bdbde6c057234de96f2
Haripal Singh Thakur <48768865+haripalthakur@users.noreply.github.com>
2025-02-11 15:13:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 // script.js
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
+// Theme Toggle
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('light-theme');
+    const isLightTheme = body.classList.contains('light-theme');
+    themeToggle.innerHTML = isLightTheme ? '<i class="fas fa-sun"></i>' : '<i class="fas fa-moon"></i>';
 });
